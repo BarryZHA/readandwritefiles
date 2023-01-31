5fd1794e5adc6916851a6c36fd72677be4ed61fe
Zheng Zhang <Zheng_Zhang1@baylor.edu>
2023-01-30 19:01:19 -0600

    update

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -6,7 +6,7 @@ outfile = open("salesreport.csv", "w", newline="")
 writer = csv.writer(outfile)
 writer.writerow(["CustomerID", "GrandTotal"])
 # Keep track of the current customer ID and GrandTotal
-current_customer = None
+current_customer = "0"
 current_total = 0
 next(csv_reader) #skip header  
 # Iterate through each row
@@ -16,7 +16,7 @@ for row in csv_reader:
     # Reset customer's ID and GrandTotal for different customers 
     if current_customer != customer_id:
         # Generate final GrandToal for each customer
-        if current_customer is not None:
+        if current_customer != "0":
             writer.writerow([current_customer, format(current_total,'.2f')])
         current_customer = customer_id
         current_total = 0
